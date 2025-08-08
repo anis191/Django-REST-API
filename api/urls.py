959f421fd6b71b38f67918bfb52e84cdc8a48953
anisul alam <anisulalam2002@gmail.com>
2025-08-08 17:12:22 +0600

    integrate SSLCommerz payment gateway

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 # from rest_framework.routers import SimpleRouter, DefaultRouter
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
-from order.views import CartViewSet, CartItemViewSet, OrderViewSet
+from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment
 from rest_framework_nested import routers
 
 # router = SimpleRouter()
@@ -27,6 +27,8 @@ urlpatterns = [
     
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+
+    path("payment/initiate/", initiate_payment, name='initiate-payment'),
 
     #Can add custom/extra urls if we need
 ]
